db78acce6c1db62208bcde3f16ad5704638c17bb
aslihancelik <aslihancelik27@gmail.com>
2024-09-26 20:38:31 -0400

    edited

--- CSC221_Chapter2_Ex7.cpp
+++ CSC221_Chapter2_Ex7.cpp
@@ -24,21 +24,21 @@ int main()
     const double OCEAN_LEVEL = 1000; //This is an example value.
 
     //Output what the ocean level is currently.
-    cout << "The current ocean level is " << OCEAN_LEVEL << " millimeters." << endl;
+    cout << "The current ocean level is " << OCEAN_LEVEL << " millimeters." << endl << endl;
 
     //Calculate the number of millimeters higher than the current level that the ocean’s level will be in 5 years.
     oceanLevelIn5Years = OCEAN_LEVEL + (OCEAN_RISE_PER_YEAR * 5);
-    cout << "The ocean level will be " << oceanLevelIn5Years << "mm in 5 years." << endl;
+    cout << "The ocean level will be " << oceanLevelIn5Years << " mm in 5 years." << endl;
     cout << "The ocean level will be " << oceanLevelIn5Years - OCEAN_LEVEL << " mm higher than the current level in 5 years." << endl << endl;
 
     //Calculate the number of millimeters higher than the current level that the ocean’s level will be in 7 years.
     oceanLevelIn7Years = OCEAN_LEVEL + (OCEAN_RISE_PER_YEAR * 7);
-    cout << "The ocean level will be " << oceanLevelIn7Years << "mm in 7 years." << endl;
+    cout << "The ocean level will be " << oceanLevelIn7Years << " mm in 7 years." << endl;
     cout << "The ocean level will be " << oceanLevelIn7Years - OCEAN_LEVEL << " mm higher than the current level in 7 years." << endl << endl;
 
     // Calculate the number of millimeters higher than the current level that the ocean’s level will be in 10 years.
     oceanLevelIn10Years = OCEAN_LEVEL + (OCEAN_RISE_PER_YEAR * 10);
-    cout << "The ocean level will be " << oceanLevelIn10Years << "mm in 10 years." << endl;
+    cout << "The ocean level will be " << oceanLevelIn10Years << " mm in 10 years." << endl;
     cout << "The ocean level will be " << oceanLevelIn10Years - OCEAN_LEVEL << " mm higher than the current level in 10 years." << endl << endl;
 
     return 0;
